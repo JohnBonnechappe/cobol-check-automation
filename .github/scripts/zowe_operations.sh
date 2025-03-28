7d8b8e4f1dbff027d79f79b6139ea79bbfc4e8ff
JohnBonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2025-03-28 16:11:41 +1100

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -12,7 +12,7 @@ else
 fi
 
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive
-    --binary-files "cobol-check-0.2.9.jar"
+    --binary-files "cobol-check-0.2.16.jar"
 
 echo "Verifying upload"
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
