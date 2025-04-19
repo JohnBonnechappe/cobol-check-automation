d8b411989f9b29255f8ed490aee0b7f6c4e08580
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2025-04-20 07:20:29 +1000

    Update zowe_operations_t.sh

--- .github/scripts/zowe_operations_t.sh
+++ .github/scripts/zowe_operations_t.sh
@@ -3,24 +3,24 @@
 
 #zowe_operations.sh
 
-LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+#LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 #tst = $(
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
+#zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
 #)
 
 #if ! tst &>/dev/null; then
-#    echo "cobolcheck directory does not exist. creating it..."
-#    zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
+    echo "cobolcheck directory does not exist. creating it..."
+    zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
 
-#    echo "uploading cobolcheck"
+    echo "uploading cobolcheck"
 
- #   zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
+    zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
  #   --binary-files "cobol-check-0.2.16.jar"  
 #else
 #    echo "cobolcheck directory already exists..."
 #fi
 
-#echo "Verifying upload"
-#zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
-echo "Done"
+echo "Verifying upload"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
+echo "Done zowe_operations"
