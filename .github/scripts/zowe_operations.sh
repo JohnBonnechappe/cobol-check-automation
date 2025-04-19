263642cac4a14b80f24d5b5627d108fd453aa6c9
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2025-04-20 08:12:58 +1000

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -13,7 +13,7 @@ if cobolcheck_exists != true; then
 
     echo "uploading cobolcheck"
 
-    zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
+    zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD \
     --binary-files "cobol-check-0.2.16.jar"  
 else
     echo "cobolcheck directory already exists..."
