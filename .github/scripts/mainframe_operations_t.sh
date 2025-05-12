045f118952c8c8a18234dbc8ed848fdb57a5db59
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2025-05-12 14:47:56 +1000

    Update mainframe_operations_t.sh

--- .github/scripts/mainframe_operations_t.sh
+++ .github/scripts/mainframe_operations_t.sh
@@ -4,4 +4,18 @@ java -version
 
 ZOWE_USERNAME = $ZOWE_USERNAME
 
-echo "in t for $ZOWE_USERNAME"
+echo "in mf_t"
+echo "for un=$ZOWE_USERNAME"
+echo "for lower_un=$LOWERCASE_USERNAME"
+
+cd cobol-check
+echo "Changed to $(pwd)"
+ls -al
+
+echo "test of numbers" >> C0099.CBL
+
+if cp CC0099.CBL "//'{$LOWERCASE_USERNAME}.CBL(NUMBERZ)'"; then
+    echo "copied program"
+else
+    echo "failed to copy program"
+fi
