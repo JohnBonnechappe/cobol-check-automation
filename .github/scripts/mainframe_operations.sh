34a1b87aa76278f073f02a08e6a34b62c89b726d
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2025-05-13 07:32:57 +1000

    Update mainframe_operations.sh

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -30,14 +30,15 @@ run_cobolcheck() {
   ./cobolcheck -p $program
   echo "******** cobolcheck completed for $program"
 
-  echo "*******test tfr"
-  echo "junk" > jnk.dat
-  zowe zos-files upload file-to-data-set "jnk.dat" "$LOWERCASE_USERNAME.CBL(NUMBER2)" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
-  echo "*******done test tfr******"
+#  echo "*******test tfr"
+#  echo "junk" > jnk.dat
+#  zowe zos-files upload file-to-data-set "jnk.dat" "$LOWERCASE_USERNAME.CBL(NUMBER2)" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD
+#  echo "*******done test tfr******"
 
   if [ -f "CC##99.CBL" ]; then
       zowe_sts=false
-      zowe zos-files upload file-to-data-set "CC##99.CBL" "$LOWERCASE_USERNAME.CBL(NUMBERZ)" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD && zowe_sts
+      zowe zos-files upload file-to-data-set "CC##99.CBL" "$LOWERCASE_USERNAME.CBL($program)" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD && zowe_sts
+      echo "Zowe status after copy=$zowe_sts"
       if zowe_sts; then
           echo "copied $program"
       else
@@ -49,10 +50,12 @@ run_cobolcheck() {
 
 #copy the jcl
   if [ -f "${program}.JCL" ]; then
-      if cp ${program}.JCL "//'{$LOWERCASE_USERNAME}.JCL($program)'"; then
-          echo "copied"
+      zowe_sts=false
+      zowe zos-files upload file-to-data-set "$program.JCL" "$LOWERCASE_USERNAME.JCL($program)" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD && zowe_sts
+      if zowe_sts; then
+          echo "copied JCL"
       else
-          echo "failed to copy"
+          echo "failed to copy JCL"
       fi
   else
       echo "${program}.jcl not found"
