3c1e7fd164aa5158e875f21c0bb40cecf71d8c1f
John Bonnechappe <108032685+JohnBonnechappe@users.noreply.github.com>
2025-05-13 07:52:26 +1000

    Update mainframe_operations.sh

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -49,6 +49,10 @@ run_cobolcheck() {
   fi
 
 #copy the jcl
+  echo "Copying the JCL"
+  echo $(pwd)
+  echo "proceeding but check first"
+  ls -la
   if [ -f "${program}.JCL" ]; then
       zowe_sts=false
       zowe zos-files upload file-to-data-set "$program.JCL" "$LOWERCASE_USERNAME.JCL($program)" --u $ZOWE_USERNAME --pw $ZOWE_PASSWORD && zowe_sts=true
